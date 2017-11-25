68588cb1a49abba15003a0139982436c4fde5545
Vivian <vivian.sarazin@gmail.com>
2017-11-25 12:15:32 +0100

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+//    Box
+///   1st track
+////  hho
+$('#s1 > div > div > div.col-9 > div:nth-child(1) > button').click(function(){
+  $(this).toggleClass('hho');
+});
+////  hhc
+$('#s1 > div > div > div.col-9 > div:nth-child(2) > button').click(function(){
+  $(this).toggleClass('hhc');
+});
+////  snare
+$('#s1 > div > div > div.col-9 > div:nth-child(3) > button').click(function(){
+  $(this).toggleClass('s');
+});
+////  kick
+$('#s1 > div > div > div.col-9 > div:nth-child(4) > button').click(function(){
+  $(this).toggleClass('k');
+});
