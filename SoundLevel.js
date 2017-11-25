548308035a18f46f1dcee52d28b1a28ff10fe671
Vivian <vivian.sarazin@gmail.com>
2017-11-25 12:33:57 +0100

    soundlevel

--- SoundLevel.js
+++ SoundLevel.js
@@ -1,1 +1,28 @@
-// no jquery
+function SoundLevel(k, s, hhc, hho, ak, as, ahhc, ahho, tempo, spot, points) {
+  this.k = k;
+  this.s = s;
+  this.hhc = hhc;
+  this.hho = hho;
+  // ...
+}
+
+function SoundLevel(music, answers) {
+  this.music = {
+    track: t,
+    artist: a,
+    year: y,
+    kick: k,
+    snare: s,
+    hhclosed: hhc,
+    hhopened: hho,
+    tempo: tempo,
+    spot: spot,
+  };
+  this.answers = {
+    answerKick: ak,
+    answerSnare: as,
+    answerHhclosed: aahc,
+    answerHhcopened: aaho,
+    points: points,
+  };
+}
