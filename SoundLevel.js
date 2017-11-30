bfc658d6b978ed52653c73c28056440aa0b9d250
Vivian <vivian.sarazin@gmail.com>
2017-11-30 20:02:03 +0100

    pas facile

--- SoundLevel.js
+++ SoundLevel.js
@@ -10,65 +10,20 @@ function SoundLevel(music, check) {
     spot: music.spot
   };
   this.answers = {
-    answerKick: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    answerSnare: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    answerHhClosed: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    answerHhOpened: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    kick: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    snare: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    hhclosed: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    hhopened: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     answerTempo: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    /*    checkKick: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-          checkSnare: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-          checkHhClosed: [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
-          checkHhOpened: [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0], */
   };
   this.check = {
     id: check.id,
-    checkKick: check.checkKick,
-    checkSnare: check.checkSnare,
-    checkHhClosed: check.checkHhClosed,
-    checkHhOpened: check.checkHhOpened,
-    points: check.pts
+    kick: check.kick,
+    snare: check.snare,
+    hhclosed: check.hhclosed,
+    hhopened: check.hhopened,
+    points: check.points
   };
-  var that = this;
-  this.i = -1;
-  /// Playmobil
-  setInterval(function() {
-    that.i = (that.i + 1) % that.music.times;
-
-    //// kick
-    if (that.answers.answerKick[that.i] === 1) {
-      new Audio(that.music.sounds.kick).play();
-    }
-    //// snare
-    if (that.answers.answerSnare[that.i] === 1) {
-      new Audio(that.music.sounds.snare).play();
-    }
-    //// hhclosed
-    if (that.answers.answerHhClosed[that.i] === 1) {
-      new Audio(that.music.sounds.hhclosed).play();
-    }
-    //// hhopened
-    if (that.answers.answerHhOpened[that.i] === 1) {
-      new Audio(that.music.sounds.hhopened).play();
-    }
-    //// tempo
-    // console.log(that.i);
-    if (that.answers.answerTempo[that.i] === 1) {
-      $("div.track.tempo > button[position=" + (that.i - 1) + "]")
-        .removeClass()
-        .addClass("box");
-      $("div.track.tempo > button[position=" + that.i + "]").toggleClass(
-        "shine"
-      );
-      if (that.i === 15) {
-        $("div.track.tempo > button[position='0']").toggleClass("shine");
-      }
-      if (that.i === 0) {
-        $("div.track.tempo > button[position='15']")
-          .removeClass()
-          .addClass("box");
-      }
-    }
-  }, 60000 / that.music.tempo / 4);
 }
 
 /// Music it's people ...
@@ -81,10 +36,10 @@ var music = [
     year: "1998",
     beatbox: "tr-909", // classe CSS
     sounds: {
-      kick: "sounds/1/k.wav",
-      snare: "sounds/1/s.wav",
-      hhclosed: "sounds/1/hhc.wav",
-      hhopened: "sounds/1/hho.wav"
+      kick: "sounds/1/kick.wav",
+      snare: "sounds/1/snare.wav",
+      hhclosed: "sounds/1/hhclosed.wav",
+      hhopened: "sounds/1/hhopened.wav"
     },
     tempo: 128,
     times: 16,
@@ -98,10 +53,10 @@ var music = [
     year: "1998",
     beatbox: "linn",
     sounds: {
-      kick: "sounds/2/k.wav",
-      snare: "sounds/2/s.wav",
-      hhclosed: "sounds/2/hhc.wav",
-      hhopened: "sounds/2/hho.wav"
+      kick: "sounds/2/kick.wav",
+      snare: "sounds/2/snare.wav",
+      hhclosed: "sounds/2/hhclosed.wav",
+      hhopened: "sounds/2/hhopened.wav"
     },
     tempo: 128,
     times: 16,
@@ -115,23 +70,21 @@ var music = [
 var check = [
   {
     id: "1", // https://www.youtube.com/watch?v=xLCVNUj40I0 //
-    checkKick: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-    checkSnare: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-    checkHhClosed: [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
-    checkHhOpened: [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+    kick: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    snare: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    hhclosed: [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+    hhopened: [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
     pts: 100
   },
   {
     id: "2",
-    checkKick: [],
-    checkSnare: [],
-    checkHhClosed: [],
-    checkHhOpened: [],
+    kick: [],
+    snare: [],
+    hhclosed: [],
+    hhopened: [],
     pts: 200
   }
 ];
-
-SoundLevel.prototype.play = function() {};
 
 SoundLevel.prototype.html = function() {
   $("#title").text(this.music.track);
@@ -161,4 +114,77 @@ SoundLevel.prototype.html = function() {
   $("#s1 > div > div > div.col-2.align-center").append(this.music.spot);
 };
 
-var sl = new SoundLevel(music[0], check[0]);
+SoundLevel.prototype.play = function() {
+  var that = this;
+  this.i = -1;
+  /// Playmobil
+  setInterval(function() {
+    that.i = (that.i + 1) % that.music.times;
+
+    //// kick
+    if (that.answers.kick[that.i] === 1) {
+      new Audio(that.music.sounds.kick).play();
+    }
+    //// snare
+    if (that.answers.snare[that.i] === 1) {
+      new Audio(that.music.sounds.snare).play();
+    }
+    //// hhclosed
+    if (that.answers.hhclosed[that.i] === 1) {
+      new Audio(that.music.sounds.hhclosed).play();
+    }
+    //// hhopened
+    if (that.answers.hhopened[that.i] === 1) {
+      new Audio(that.music.sounds.hhopened).play();
+    }
+    //// tempo
+    // console.log(that.i);
+    if (that.answers.answerTempo[that.i] === 1) {
+      $("div.track.tempo > button[position=" + (that.i - 1) + "]")
+        .removeClass()
+        .addClass("box");
+      $("div.track.tempo > button[position=" + that.i + "]").toggleClass(
+        "shine"
+      );
+      if (that.i === 15) {
+        $("div.track.tempo > button[position='0']").toggleClass("shine");
+      }
+      if (that.i === 0) {
+        $("div.track.tempo > button[position='15']")
+          .removeClass()
+          .addClass("box");
+      }
+    }
+  }, 60000 / that.music.tempo / 4);
+};
+
+SoundLevel.prototype.track = function() {
+  var arrayKeys = Object.keys(this.answers);
+  for (var i = 0; i < arrayKeys.length; i++) {
+    if (this.answers[arrayKeys[i]][$(this).attr("position")] == 0) {
+      new Audio(
+        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/" +
+          this.music.id +
+          "/hho.wav"
+      ).play();
+      this.answers[i].splice($(this).attr("position"), 1, 1);
+      console.log(this.answers[i].splice($(this).attr("position"), 1, 1));
+    } else if (this.answers[arrayKeys[i]][$(this).attr("position")] == 1) {
+      this.answers[i].splice($(this).attr("position"), 1, 0);
+    }
+  }
+
+  // $("#tracks > div.track0 > button").click(function() {
+  //   $(this).toggleClass("hho");
+  //   if (this.answers.answerHhOpened[$(this).attr("position")] == 0) {
+  //     new Audio(
+  //       "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/hho.wav"
+  //     ).play();
+  //     this.answers.answerHhOpened.splice($(this).attr("position"), 1, 1);
+  //   } else if (this.answers.answerHhOpened[$(this).attr("position")] == 1) {
+  //     this.answers.answerHhOpened.splice($(this).attr("position"), 1, 0);
+  //   }
+};
+
+var sl1 = new SoundLevel(music[0], check[0]);
+var sl2 = new SoundLevel(music[1], check[1]);
--- script.js
+++ script.js
@@ -1,33 +1,126 @@
 //    Start game
 $("#go").click(function() {
-  sl.html();
-  
-  // $("#title").text(sl.music.track);
-  // $("#artist").text(sl.music.artist);
-  // $("#year").text(sl.music.year);
-  // var html = "";
-  // for (var j = 0; j < Object.keys(sl.music.sounds).length; j++) {
-  //   html += '<div class="track' + j + '"><img src="">';
-  //   for (var i = 0; i < sl.music.times; i++) {
-  //     html += '<button class="box" position="' + i + '"></button>';
+  sl1.play();
+  sl1.html();
+  sl1.track();
+  //    Box
+  ///   1st track
+  ////  hho
+  // $("#tracks > div.track0 > button").click(function() {
+  //   $(this).toggleClass("hho");
+  //   if (sl1.answers.answerHhOpened[$(this).attr("position")] == 0) {
+  //     new Audio(
+  //       "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/hho.wav"
+  //     ).play();
+  //     sl1.answers.answerHhOpened.splice($(this).attr("position"), 1, 1);
+  //   } else if (sl1.answers.answerHhOpened[$(this).attr("position")] == 1) {
+  //     sl1.answers.answerHhOpened.splice($(this).attr("position"), 1, 0);
   //   }
-  //   html +=
-  //     '<img src="http://cdn.onlinewebfonts.com/svg/img_237800.png" class="item align-top nope">';
-  //   html += "</div>";
-  // }
+  // });
+  // ////  hhc
+  // $("#tracks > div.track1 > button").click(function() {
+  //   $(this).toggleClass("hhc");
+  //   if (sl1.answers.answerHhClosed[$(this).attr("position")] == 0) {
+  //     new Audio(
+  //       "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/hhc.wav"
+  //     ).play();
+  //     sl1.answers.answerHhClosed.splice($(this).attr("position"), 1, 1);
+  //   } else if (sl1.answers.answerHhClosed[$(this).attr("position")] == 1) {
+  //     sl1.answers.answerHhClosed.splice($(this).attr("position"), 1, 0);
+  //   }
+  // });
+  // ////  snare
+  // $("#tracks > div.track2 > button").click(function() {
+  //   $(this).toggleClass("s");
+  //   if (sl1.answers.answerSnare[$(this).attr("position")] == 0) {
+  //     new Audio(
+  //       "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/s.wav"
+  //     ).play();
+  //     sl1.answers.answerSnare.splice($(this).attr("position"), 1, 1);
+  //   } else if (sl1.answers.answerSnare[$(this).attr("position")] == 1) {
+  //     sl1.answers.answerSnare.splice($(this).attr("position"), 1, 0);
+  //   }
+  // });
+  // ////  kick
+  // $("#tracks > div.track3 > button").click(function() {
+  //   $(this).toggleClass("k");
+  //   if (sl1.answers.answerKick[$(this).attr("position")] == 0) {
+  //     new Audio(
+  //       "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/k.wav"
+  //     ).play();
+  //     sl1.answers.answerKick.splice($(this).attr("position"), 1, 1);
+  //   } else if (sl1.answers.answerKick[$(this).attr("position")] == 1) {
+  //     sl1.answers.answerKick.splice($(this).attr("position"), 1, 0);
+  //   }
+  // });
 
-  // html += '<div class="track tempo"><img>';
-  // for (var k = 0; k < sl.music.times; k++) {
-  //   html += '<button class="box yolo" position="' + k + '"></button>';
-  // }
-  // html += "</div>";
-  // $("#tracks").append(html);
+  ///bds
+  $("#bds").click(function() {
+    sl1.answers = {
+      answerKick: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      answerSnare: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      answerHhClosed: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      answerHhOpened: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      answerTempo: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    };
+    $("button.box")
+      .removeClass()
+      .addClass("box");
+  });
 
-  // $("#s1 > div > div > div.col-lg-12.text-right").append(
-  //   "<h2>0</h2><h3>points</h3>"
-  // );
-  // $("#s1 > div > div > div.col-2.align-center").append(sl.music.spot);
+  ///check
+  setInterval(function() {
+    ////hhopened
+    if (_.isEqual(sl1.answers.answerHhOpened, check[0].checkHhOpened))
+      $("#tracks > div.track0 > img.nope")
+        .removeClass()
+        .addClass("item align-top");
+    else if (!_.isEqual(sl1.answers.answerHhOpened, check[0].checkHhOpened))
+      $("#tracks > div.track0 > img.nope")
+        .removeClass()
+        .addClass("nope");
+    ////hhclosed
+    if (_.isEqual(sl1.answers.answerHhClosed, check[0].checkHhClosed))
+      $("#tracks > div.track1 > img.nope")
+        .removeClass()
+        .addClass("item align-top");
+    else if (!_.isEqual(sl1.answers.answerHhClosed, check[0].checkHhClosed))
+      $("#tracks > div.track1 > img.nope")
+        .removeClass()
+        .addClass("nope");
+    ////snare
+    if (_.isEqual(sl1.answers.answerSnare, check[0].checkSnare))
+      $("#tracks > div.track2 > img.nope")
+        .removeClass()
+        .addClass("item align-top");
+    else if (!_.isEqual(sl1.answers.answerSnare, check[0].checkSnare))
+      $("#tracks > div.track2 > img.nope")
+        .removeClass()
+        .addClass("nope");
+    ////kick
+    if (_.isEqual(sl1.answers.answerKick, check[0].checkKick))
+      $("#tracks > div.track3 > img.nope")
+        .removeClass()
+        .addClass("item align-top");
+    else if (!_.isEqual(sl1.answers.answerKick, check[0].checkKick))
+      $("#tracks > div.track3 > img.nope")
+        .removeClass()
+        .addClass("nope");
+    if (
+      _.isEqual(sl1.answers.answerHhOpened, check[0].checkHhOpened) &&
+      _.isEqual(sl1.answers.answerHhClosed, check[0].checkHhClosed) &&
+      _.isEqual(sl1.answers.answerSnare, check[0].checkSnare) &&
+      _.isEqual(sl1.answers.answerKick, check[0].checkKick)
+    ) {
+      $("#gg1").removeClass("nope");
+      $("#s1 > div > div > div.col-lg-12.text-right > h2").text(check[0].pts);
+    }
+  }, 100);
+});
 
+$('#gg1').click(function(){
+  sl2.play();
+  sl2.html();
   //    Box
   ///   1st track
   ////  hho
@@ -35,7 +128,7 @@ $("#go").click(function() {
     $(this).toggleClass("hho");
     if (sl.answers.answerHhOpened[$(this).attr("position")] == 0) {
       new Audio(
-        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/hho.wav"
+        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/2/hho.wav"
       ).play();
       sl.answers.answerHhOpened.splice($(this).attr("position"), 1, 1);
     } else if (sl.answers.answerHhOpened[$(this).attr("position")] == 1) {
@@ -47,7 +140,7 @@ $("#go").click(function() {
     $(this).toggleClass("hhc");
     if (sl.answers.answerHhClosed[$(this).attr("position")] == 0) {
       new Audio(
-        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/hhc.wav"
+        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/2/hhc.wav"
       ).play();
       sl.answers.answerHhClosed.splice($(this).attr("position"), 1, 1);
     } else if (sl.answers.answerHhClosed[$(this).attr("position")] == 1) {
@@ -59,7 +152,7 @@ $("#go").click(function() {
     $(this).toggleClass("s");
     if (sl.answers.answerSnare[$(this).attr("position")] == 0) {
       new Audio(
-        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/s.wav"
+        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/2/s.wav"
       ).play();
       sl.answers.answerSnare.splice($(this).attr("position"), 1, 1);
     } else if (sl.answers.answerSnare[$(this).attr("position")] == 1) {
@@ -71,7 +164,7 @@ $("#go").click(function() {
     $(this).toggleClass("k");
     if (sl.answers.answerKick[$(this).attr("position")] == 0) {
       new Audio(
-        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/1/k.wav"
+        "/home/vivian/Bureau/IRON HACK/2_PROJECTS/beatmeista/sounds/2/k.wav"
       ).play();
       sl.answers.answerKick.splice($(this).attr("position"), 1, 1);
     } else if (sl.answers.answerKick[$(this).attr("position")] == 1) {
